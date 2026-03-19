4c176cfd7751459477c2a6938d44bb340d0db064
onyx-and-iris <code@onyxandiris.online>
2026-03-19 03:58:44 +0000

    patch bump
    upd ruff config

--- buildVars.py
+++ buildVars.py
@@ -28,7 +28,7 @@ addon_info = {
 The add-on requires Voicemeeter to be installed."""
     ),
     # version
-    'addon_version': '1.1.0',
+    'addon_version': '1.1.1',
     # Author(s)
     'addon_author': 'onyx-and-iris <code@onyxandiris.online>',
     # URL for the add-on documentation support
