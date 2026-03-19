c1b374cce75f016a9004337197c1d01fd9657118
onyx-and-iris <code@onyxandiris.online>
2026-03-19 06:17:55 +0000

    patch bump

--- buildVars.py
+++ buildVars.py
@@ -28,7 +28,7 @@ addon_info = {
 The add-on requires Voicemeeter to be installed."""
     ),
     # version
-    'addon_version': '1.2.0',
+    'addon_version': '1.2.1',
     # Author(s)
     'addon_author': 'onyx-and-iris <code@onyxandiris.online>',
     # URL for the add-on documentation support
